dc988f4bfa5139c80786dcc24654529eb88fd147
dkifu < GitHubtubpong_big@hotmail.com>
2026-09-21 17:56:03 +0700

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/blacklist-narathiwat/',
+  base: './',
 
   server: {
     host: '0.0.0.0',
