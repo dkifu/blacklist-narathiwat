a347abda92a38841716788486637dc33dab3618c
dkifu < GitHubtubpong_big@hotmail.com>
2026-09-21 17:49:54 +0700

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  base: '/blacklist-narathiwat/',
+
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['.trycloudflare.com'],
+    allowedHosts: [
+      '.trycloudflare.com',
+    ],
   },
 })
